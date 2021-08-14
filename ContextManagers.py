ab4847e1ecc5aa5a6ce882176513ba189b49f11c
yusseef <yussef.raouf11@gmail.com>
2021-08-14 05:14:29 +0200

    context_manager

---
+++ ContextManagers.py
@@ -0,0 +1,26 @@
+#Example 1 
+with open('note.txt', 'w') as file: #This will make sure that the file closed after the editing
+    file.write('Hello')    
+
+#Example 2 
+from threading import Lock
+lock = Lock()
+with lock: #Dont need to do lock.release()
+    #.....
+    pass
+
+#Example 3 in classes
+
+class ManagedFile:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __enter__(self):
+        print('enter')
+        self.file = open(self.filename, 'w')
+        return self.file
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if self.file:
+            file.close()
+        print('exit')
+
--- Exceptions.py
+++ Exceptions.py
@@ -16,6 +16,40 @@ if x < 0:
 assert(x <= 0) , 'Negative value' #AssertionError
 
 try:
-    a = 5 / 0
+    a = 5 / 0 #No divide by zero allowed
 except :
     print('error')
+
+
+try:
+    a = 6 / 0
+    b = a + '10'
+except Exception as e:
+    print(e) #Will print the error as a string
+
+except TypeError:
+    print('error') #You can determine the error type
+
+else: #Runs when there is no errors
+    print('no error')
+
+finally:#Runs always
+    print('always run')
+
+
+'''define ur own exceptions'''
+
+class TooHighValueError(Exception):
+    pass
+
+def test(x):
+    if x > 500:
+        raise TooHighValueError('VAlue is too high')
+
+test(700) #Return VAlue is too high
+
+
+try:
+    test(700)
+except TooHighValueError:
+    print('Value is too high') #Return Value is too high
